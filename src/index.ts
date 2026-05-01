5ee9ee1a814eafcfa8601ff7547317a98741f258
manish sharma <msmanish407@gmail.com>
2026-05-01 16:05:23 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Hello, welcome to the Classroom API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serer is running on http://localhost:${PORT}`);
+});
